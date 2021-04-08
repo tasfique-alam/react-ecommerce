b1c8528777a812dea726f03fe71c43a9d18dd016
MH Shakil <shakil5078@gmail.com>
2021-04-09 02:01:56 +0600

    sidebar

--- src/layouts/Public/Sidebar/index.js
+++ src/layouts/Public/Sidebar/index.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Form } from "react-bootstrap";
+const Sidebar = () => {
+  return (
+    <div className="product-sidebar-area">
+      <div className="d-flex justify-content-between">
+        <label className="mb-0">Brand</label>
+        <MdKeyboardArrowDown className="align-self-center" />
+      </div>
+      <Form.Control type="text" placeholder="Search by Brands" />
+    </div>
+  );
+};
 
- const Sidebar = () => {
-    return (
-        <div>
-            Sidebar
-        </div>
-    )
-}
-
-export default Sidebar
+export default Sidebar;
--- src/pages/AllProduct/index.js
+++ src/pages/AllProduct/index.js
@@ -1,39 +1,47 @@
 import React from "react";
 import { Row, Col, Card } from "react-bootstrap";
+import Sidebar from "../../layouts/Public/Sidebar";
 
 const Product = () => {
   return (
     <div className="product-area">
       <Row>
-        <Col xs={6} md={3}>
-          <Card className="border-0 card-container" style={{ width: "18rem" }}>
-            <Card.Img
-              className="image-container"
-              variant="top"
-              src="images/8e-300x300.png"
-            />
-            <Card.Body className="card-content">
-              <Card.Text className="w-75 mb-1">
-                Multicolor Cotton long sleeve shirt for men
-              </Card.Text>
-              <h5>BDT. 7,850</h5>
-              <div className="d-flex justify-content-between">
-                <p className="product-price">
-                  <del>BDT. 7,850</del>
-                </p>
-                <p className="product-price">34% Off</p>
-              </div>
-            </Card.Body>
-          </Card>
+        <Col lg={2}>
+          <Sidebar />
         </Col>
-        <Col xs={6} md={3}>
-          xs=6 md=4
-        </Col>
-        <Col xs={6} md={3}>
-          xs=6 md=4
-        </Col>
-        <Col xs={6} md={3}>
-          xs=6 md=4
+        <Col lg={8}>
+          <Row>
+            <Col xs={6} md={3}>
+              <Card className="border-0 card-container">
+                <Card.Img
+                  className="image-container img-fluid"
+                  variant="top"
+                  src="images/8e-300x300.png"
+                />
+                <Card.Body className="card-content">
+                  <Card.Text className=" mb-1">
+                    Multicolor Cotton long sleeve shirt for men
+                  </Card.Text>
+                  <h5>BDT. 7,850</h5>
+                  <div className="d-flex justify-content-between">
+                    <p className="product-price">
+                      <del>BDT. 7,850</del>
+                    </p>
+                    <p className="product-price">34% Off</p>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={6} md={3}>
+              xs=6 md=4
+            </Col>
+            <Col xs={6} md={3}>
+              xs=6 md=4
+            </Col>
+            <Col xs={6} md={3}>
+              xs=6 md=4
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
