f3ff858370b9f8796a64e96159f9b8e0fc5e5c5c
MH Shakil <shakil5078@gmail.com>
2021-04-09 01:18:41 +0600

    product done

--- src/pages/AllProduct/index.js
+++ src/pages/AllProduct/index.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
 
- const Product = () => {
-    return (
-        <div>
-            Product
-        </div>
-    )
-}
+const Product = () => {
+  return (
+    <div className="product-area">
+      <Row>
+        <Col xs={6} md={3}>
+          <Card className="border-0 card-container" style={{ width: "18rem" }}>
+            <Card.Img
+              className="image-container"
+              variant="top"
+              src="images/8e-300x300.png"
+            />
+            <Card.Body className="card-content">
+              <Card.Text className="w-75 mb-1">
+                Multicolor Cotton long sleeve shirt for men
+              </Card.Text>
+              <h5>BDT. 7,850</h5>
+              <div className="d-flex justify-content-between">
+                <p className="product-price">
+                  <del>BDT. 7,850</del>
+                </p>
+                <p className="product-price">34% Off</p>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={6} md={3}>
+          xs=6 md=4
+        </Col>
+        <Col xs={6} md={3}>
+          xs=6 md=4
+        </Col>
+        <Col xs={6} md={3}>
+          xs=6 md=4
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
